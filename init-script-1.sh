0f9971849fe2b613d649ca8f27d3fc6bdd37779f
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2024-04-19 21:16:26 -0300

    Commit

--- init-script-1.sh
+++ init-script-1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 yum update -y
+yum -y remove httpd 
+yum -y remove httpd-tools 
 yum install -y php-fpm php-curl php-cli php-json php-mysql php-opcache php-dom php-exif php-fileinfo php-zip php-mbstring php-hash php-imagick php-openssl php-pcre php-xml php-bcmath php-filter php-pear php-gd php-mcrypt php-intl php-iconv php-zlib php-xmlreader
-yum install ngnix -y
+yum install -y ngnix
 
